ed49d4b5d556c91b9c618bd115ffa82b5737b603
josephgs-koo <josephgs.koo@gmail.com>
2023-03-08 22:25:10 +0900

    [init]초기 세팅 완료

--- client/src/Btn.tsx
+++ client/src/Btn.tsx
@@ -7,7 +7,7 @@ type prop = {
 
 export default function Btn({ title, handler }: prop) {
   return (
-    <div className="border-[2px] border-default-pink" onClick={handler}>
+    <div className="border-[2px] border-[#ff3366]" onClick={handler}>
       {title}
     </div>
   );
