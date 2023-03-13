5fe28e279a5fab21f45a613af7e06b9e83bf1bf3
eunah-J <kdsmd62@gmail.com>
2023-03-13 21:48:36 +0900

    [fix] 닉네임 중복 체크 기능 추가
    - 닉네임 중복 체크 기능 추가
    - 비밀번호 정규식 패턴 수정

--- server/src/main/java/com/seb006/server/member/dto/MemberDto.java
+++ server/src/main/java/com/seb006/server/member/dto/MemberDto.java
@@ -16,7 +16,7 @@ public class MemberDto {
         private String email;
 
         @NotBlank
-        @Pattern(regexp = "^[A-Za-z0-9]{8,20}$",
+        @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d~!@#$%^&*()+|=]{8,20}$",
                 message = "비밀번호는 숫자, 문자를 포함하여 8~20자리여야 합니다.")
         private String password;
 
