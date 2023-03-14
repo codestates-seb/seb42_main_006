b30c9d194131442d033866d798c0d6ca6b134ff1
“cherry2030” <cherry2030@naver.com>
2023-03-15 01:01:57 +0900

    [feat] @DynamicUpdate 어노테이션 추가

--- server/src/main/java/com/seb006/server/prfpost/entity/PrfPost.java
+++ server/src/main/java/com/seb006/server/prfpost/entity/PrfPost.java
@@ -9,12 +9,13 @@ import com.seb006.server.url.entity.Urls;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@DynamicUpdate
 @NoArgsConstructor
 @Getter
 @Setter
