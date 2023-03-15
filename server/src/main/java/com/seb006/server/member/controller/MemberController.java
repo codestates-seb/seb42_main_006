367cb2e1d6d40dbee224341573127a8e9b7d829d
eunah-J <kdsmd62@gmail.com>
2023-03-15 15:27:24 +0900

    [fix] Pagination 적용

--- server/src/main/java/com/seb006/server/member/controller/MemberController.java
+++ server/src/main/java/com/seb006/server/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.seb006.server.member.controller;
 
+import com.seb006.server.global.response.MultiResponseDto;
 import com.seb006.server.like.entity.PrfPostLike;
 import com.seb006.server.member.dto.MemberDto;
 import com.seb006.server.member.dto.MemberPostsDto;
@@ -10,15 +11,19 @@ import com.seb006.server.member.service.MemberService;
 import com.seb006.server.prfpost.entity.PrfPost;
 import com.seb006.server.recruitpost.entity.RecruitPost;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
 import java.security.Principal;
 import java.util.List;
 
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/members")
 public class MemberController {
@@ -89,26 +94,41 @@ public class MemberController {
     }
 
     @GetMapping("/prf-post")
-    public ResponseEntity getMyPrfPosts(Principal principal) {
-        List<PrfPost> prfPosts =  memberPostsService.findMyPrfPosts(principal.getName());
+    public ResponseEntity getMyPrfPosts(Principal principal,
+                                        @Positive @RequestParam(defaultValue = "1") int page,
+                                        @Positive @RequestParam(defaultValue = "10") int size) {
+        Page<PrfPost> prfPostPage
+                = memberPostsService.findMyPrfPosts(principal.getName(), page - 1, size);
+        List<PrfPost> prfPosts =  prfPostPage.getContent();
         List<MemberPostsDto> response = memberMapper.prfPostsToMemberPostsDtos(prfPosts);
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(
+                new MultiResponseDto<>(response, prfPostPage), HttpStatus.OK);
     }
 
     @GetMapping("/recruit-posts")
-    public ResponseEntity getMyRecruitPosts(Principal principal) {
-        List<RecruitPost> recruitPosts = memberPostsService.findMyRecruitPosts(principal.getName());
+    public ResponseEntity getMyRecruitPosts(Principal principal,
+                                            @Positive @RequestParam(defaultValue = "1") int page,
+                                            @Positive @RequestParam(defaultValue = "10") int size) {
+        Page<RecruitPost> recruitPostPage
+                = memberPostsService.findMyRecruitPosts(principal.getName(), page - 1, size);
+        List<RecruitPost> recruitPosts = recruitPostPage.getContent();
         List<MemberPostsDto> response = memberMapper.recruitPostsToMemberPostsDtos(recruitPosts);
 
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return new ResponseEntity<>(
+                new MultiResponseDto<>(response, recruitPostPage), HttpStatus.OK);
     }
 
     @GetMapping("/prf-posts-like")
-    public ResponseEntity getMyPrfPostsLike(Principal principal) {
-        List<PrfPostLike> prfPostLikes = memberPostsService.findMyPrfPostsLike(principal.getName());
+    public ResponseEntity getMyPrfPostsLike(Principal principal,
+                                            @Positive @RequestParam(defaultValue = "1") int page,
+                                            @Positive @RequestParam(defaultValue = "10") int size) {
+        Page<PrfPostLike> prfPostLikePage
+                = memberPostsService.findMyPrfPostsLike(principal.getName(), page - 1, size);
+        List<PrfPostLike> prfPostLikes = prfPostLikePage.getContent();
         List<MemberPostsDto> response = memberMapper.prfPostsLikeToMemberPostDtos(prfPostLikes);
 
-        return new ResponseEntity(response, HttpStatus.OK);
+        return new ResponseEntity(
+                new MultiResponseDto<>(response, prfPostLikePage), HttpStatus.OK);
     }
 }
--- server/src/main/java/com/seb006/server/member/service/MemberPostsService.java
+++ server/src/main/java/com/seb006/server/member/service/MemberPostsService.java
@@ -5,10 +5,13 @@ import com.seb006.server.member.entity.Member;
 import com.seb006.server.member.repository.MemberRepository;
 import com.seb006.server.prfpost.entity.PrfPost;
 import com.seb006.server.recruitpost.entity.RecruitPost;
+import com.seb006.server.utils.CustomPagination;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Transactional
 @Service
@@ -21,22 +24,41 @@ public class MemberPostsService {
         this.memberService = memberService;
     }
 
-    public List<PrfPost> findMyPrfPosts(String email) {
+    public Page<PrfPost> findMyPrfPosts(String email, int page, int size) {
         Member findMember = memberService.findVerifiedMember(email);
+        List<PrfPost> prfPosts = findMember.getPrfPosts();
 
-        return findMember.getPrfPosts();
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), prfPosts.size());
+
+        Page<PrfPost> prfPostPage =
+                new PageImpl<>(prfPosts.subList(start, end), pageable, prfPosts.size());
+
+        return prfPostPage;
     }
 
-    public List<RecruitPost> findMyRecruitPosts(String email) {
+    public Page<RecruitPost> findMyRecruitPosts(String email, int page, int size) {
         Member findMember = memberService.findVerifiedMember(email);
+        List<RecruitPost> recruitPosts = findMember.getRecruitPosts();
 
-        return findMember.getRecruitPosts();
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id").descending());
+        int start = (int) pageable.getOffset();
+        int end = Math.min((start + pageable.getPageSize()), recruitPosts.size());
+
+        Page<RecruitPost> recruitPostPage =
+                new PageImpl<>(recruitPosts.subList(start, end), pageable, recruitPosts.size());
+
+        return recruitPostPage;
     }
 
-    public List<PrfPostLike> findMyPrfPostsLike(String email) {
+    public Page<PrfPostLike> findMyPrfPostsLike(String email, int page, int size) {
         Member findMember = memberService.findVerifiedMember(email);
+        List<PrfPostLike> prfPostLikes = findMember.getPrfPostLikes();
 
-        return findMember.getPrfPostLikes();
+        Page<PrfPostLike> prfPostLikePage = new CustomPagination<PrfPostLike>().pagination(prfPostLikes, page, size);
+
+        return prfPostLikePage;
     }
 
 }
