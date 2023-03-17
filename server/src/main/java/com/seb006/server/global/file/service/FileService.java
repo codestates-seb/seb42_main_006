0f27ec4dd739ac51feb0a8f87b42e94d57e19021
eunah-J <kdsmd62@gmail.com>
2023-03-17 17:26:59 +0900

    [fix] 오류 해결

--- server/src/main/java/com/seb006/server/global/file/service/FileService.java
+++ server/src/main/java/com/seb006/server/global/file/service/FileService.java
@@ -3,6 +3,7 @@ package com.seb006.server.global.file.service;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.AmazonS3Exception;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -26,30 +27,24 @@ public class FileService {
     private String bucket;
 
     public String upload(MultipartFile multipartFile, String dirName) throws IOException {
-        File uploadFile = convert(multipartFile)
-                .orElseThrow(() -> new IllegalArgumentException("Fail Convert MultipartFile to File"));
+        String fileName = dirName + "/" + multipartFile.getOriginalFilename();
+        String uploadImageUrl = putS3(multipartFile, fileName);
 
-        return upload(uploadFile, dirName);
-    }
-
-    private String upload(File uploadFile, String dirName) {
-        String fileName = dirName + "/" + uploadFile.getName();
-
-        // 업로드 된 파일의 S3 url 주소
-        String uploadImageUrl = putS3(uploadFile, fileName);
-
-        // 로컬에 생성된 File 삭제
-        removeNewFile(uploadFile);
-
-        // fileName 리턴
         return fileName;
     }
 
-    private String putS3(File uploadFile, String fileName) {
-        amazonS3Client.putObject(
-                new PutObjectRequest(bucket, fileName, uploadFile)
-                        .withCannedAcl(CannedAccessControlList.PublicRead)
-        );
+    private String putS3(MultipartFile multipartFile, String fileName) {
+        ObjectMetadata metadata = new ObjectMetadata();
+        metadata.setContentType(multipartFile.getContentType());
+        metadata.setContentLength(multipartFile.getSize());
+        try {
+            amazonS3Client.putObject(
+                    new PutObjectRequest(bucket, fileName, multipartFile.getInputStream(), metadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead)
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return amazonS3Client.getUrl(bucket, fileName).toString();
     }
 
