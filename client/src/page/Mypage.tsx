584d27430babc3ac5b5fc9527e2f0c94b14c3e33
annkim7 <ilc7748@gmail.com>
2023-03-28 08:09:50 +0900

    design: 글 색상  가시성 개선

--- client/src/page/Mypage.tsx
+++ client/src/page/Mypage.tsx
@@ -176,6 +176,8 @@ export default function Mypage() {
     )
   );
 
+  console.log(listData);
+
   const handleTab = (item: string, url: string) => {
     setTab(item);
     setUrl(`${url}?page=1&size=10`);
